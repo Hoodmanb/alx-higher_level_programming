c1332708d9eda52cad41b8ebd3b037200a28b5a9
hoodmanb <joshuadebravo@gmail.com>
2024-01-07 12:15:26 +0100

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,7 @@
 
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return None
     else:
-        _list = '{:d}'.format(my_list[idx])
-        return _list
+        return my_list[idx]
