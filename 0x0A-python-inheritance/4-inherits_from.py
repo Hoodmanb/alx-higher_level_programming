8e1ad16f7273b2494b7a41d5d638efac073171fa
hoodmanb <joshuadebravo@gmail.com>
2024-02-06 04:29:05 +0100

    (added) 4-inherits_from.py file

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""function that returns True if the object is an
+instance of a class that inherited (directly or in
+directly) from the specified class ; otherwise false"""
+
+
+def inherits_from(obj, a_class):
+    """
+    inherits_from: Check if is inherited for specificed class
+    Args:
+    obj: object
+    a_class: class
+    """
+    if isinstance(obj, a_class):
+        if issubclass(a_class, obj.__class__) is not True:
+            return True
+    return False
