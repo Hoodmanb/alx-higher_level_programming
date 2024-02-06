f6049c8d2361a92564d4d66a58b919ce05b45826
hoodmanb <joshuadebravo@gmail.com>
2024-02-06 22:28:03 +0100

    (added)1-write_file.py file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""a function that writes to a file and return"""
+
+
+def write_file(filename="", text=""):
+    """open a file, wtite to it and return the num of char wtitten"""
+    with open(filename, 'w', encoding='utf-8')
+    as file:
+        return file.write(text)
