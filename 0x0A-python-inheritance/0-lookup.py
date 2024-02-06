cbf4de742297a02d7182a2c421f842b760208fb6
hoodmanb <joshuadebravo@gmail.com>
2024-02-06 03:25:53 +0100

    (added) first file projext added

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Function that returns the list of available attributes and objects"""
+
+
+def lookup(obj):
+    """check for available attributes and methods
+    """
+    print(dir(obj))
