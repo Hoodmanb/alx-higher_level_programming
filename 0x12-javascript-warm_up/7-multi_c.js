8d8c058fabaf669ad7b4951d0e66d7f9b2f830d9
hoodmanb <joshuadebravo@gmail.com>
2024-04-08 19:02:59 +0100

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 const x = parseInt(process.argv[2]);
-if (parseInt(process.argv[2])) {
-  for(i = 0; i < x; i++) {
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for(let i = 0; i < x; i++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
