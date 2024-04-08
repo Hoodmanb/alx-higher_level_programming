9bac315ef7cb7465269d0978444f82c8fd16421a
hoodmanb <joshuadebravo@gmail.com>
2024-04-08 19:12:57 +0100

    update2

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const x = process.argv[2];
 
-if (isNaN(x)) {
+const x = process.argv[2];
+if (isNaN(Number(x))) {
   console.log('Missing number of occurrences');
 } else {
-  for(let i = 0; i < x; i++) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
