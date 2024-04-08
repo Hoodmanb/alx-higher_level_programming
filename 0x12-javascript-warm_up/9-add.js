206e12950ac57172ba07e0b57cdb1b6af3b0528d
hoodmanb <joshuadebravo@gmail.com>
2024-04-08 19:47:25 +0100

    update

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 function add(a, b) {
-  console.log(parseInt(a) + parseInt(b));
+  return console.log(parseInt(a) + parseInt(b));
 }
 
-let a = process.argv[2];
-let b = process.argv[3];
+let a = Number(process.argv[2]);
+let b = Number(process.argv[3]);
 
 add(a, b);
