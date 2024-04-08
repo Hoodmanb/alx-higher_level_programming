4394439ec0403dcf0c0a7f860bab12557dad9da5
hoodmanb <joshuadebravo@gmail.com>
2024-04-08 18:48:59 +0100

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-let i = 0;
+const x = parseInt(process.argv[2]);
 if (parseInt(process.argv[2])) {
-  while(i < parseInt(process.argv[2])){
-  console.log('C is fun');
-  i++;
+  for(i = 0; i < x; i++) {
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
