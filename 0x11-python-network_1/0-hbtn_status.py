c01ef300634ea6ee2fc7e98fbd00086d7e3ae87a
hoodmanb <joshuadebravo@gmail.com>
2024-04-29 11:12:24 +0100

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urlopen(request) as res:
         response = res.read()
         print("Body response:")
-        print(" - type:", type(response))
-        print(" - content:", response)
-        print(" - utf8 content:", response.decode("utf-8"))
+        print("    - type:", type(response))
+        print("    - content:", response)
+        print("    - utf8 content:", response.decode("utf-8"))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""A python script that takes in a URL and email
+"""A python script that takks in a URL and email
    send a post request with the email as a parameter
-   and displays the body of the response
+   and display the body of the response
 """
 from urllib.request import urlopen, Request
 from urllib.parse import urlencode
